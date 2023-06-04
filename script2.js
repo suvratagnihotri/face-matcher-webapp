37cb149112f1e07042ff070267cb2ea3f75564d8
suvrat <suvrat72000@gmail.com>
2023-06-04 20:59:54 +0530

    Face detection commit

---
+++ script2.js
@@ -0,0 +1,40 @@
+const video = document.getElementById("video");
+
+// Load the face detection and landmark models
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+]).then(startWebcam);
+
+function startWebcam() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+video.addEventListener("play", async () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video).withFaceLandmarks();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw bounding boxes around the detected faces
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+  }, 100);
+});
