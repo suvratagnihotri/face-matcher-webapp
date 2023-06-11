728083005b2d116bf404b8fdc5ce65f1cfcc5131
suvrat <suvrat72000@gmail.com>
2023-06-11 22:51:01 +0530

    Update commit

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 const video = document.getElementById("video");
-
+//starting video stream
 Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
